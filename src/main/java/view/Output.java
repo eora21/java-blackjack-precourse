308fff4f977a8ce15cc9c040ac37514fe686b437
eora21 <eora21@naver.com>
2022-12-13 13:51:44 +0900

    feat: 🎸 사용자마다의 배팅 금액 요청을 출력하는 메서드

--- src/main/java/view/Output.java
+++ src/main/java/view/Output.java
@@ -6,6 +6,10 @@ public class Output {
         print("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
     }
 
+    public void requireBettingMoney(String playerName) {
+        print(String.format("%s의 배팅 금액은?", playerName));
+    }
+
     private void print(String text) {
         System.out.println(text);
     }
